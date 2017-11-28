09dd43db09778957551a6eafacf375758073871b
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-11-28 19:25:12 +0100

    add variables to ex2game.js

---
+++ ex2game.js
@@ -0,0 +1,19 @@
+var randomNumber = Math.ceil(Math.random() * 10);
+var player = prompt('Podaj liczbę od 1 do 3');
+var papier = 1,
+    nozyce = 2,
+    kamien = 3;
+
+console.log(randomNumber);
+
+console.log('Podałeś liczbę: ' + player);
+
+if(player === 1 || randomNumber !== 1){
+    console.log('Wygrałeś');
+
+}
+
+
+
+
+
