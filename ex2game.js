fd4cbc75d049a6bbdb9b8307ff7a910410eb4ff4
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-06 20:42:19 +0100

    Changes i ex2game.js.

--- ex2game.js
+++ ex2game.js
@@ -1,23 +1,33 @@
 var randomNumber = Math.ceil(Math.random() * 10);
 var player = prompt('Podaj liczbę od 1 do 3. Legenda: 1= papier, 2= nożyce, 3= kamień');
 
+if (player > 3) {
+    alert('Podałeś złą liczbę');
+    player = prompt('Podaj liczbę od 1 do 3. Legenda: 1= papier, 2= nożyce, 3= kamień');
+
+}
+
+
 console.log('Komputer podał liczbę: ' + randomNumber);
 
 console.log('Podałeś liczbę: ' + player);
 
-if(player == 1 && randomNumber == 3){
+if (player == 1 && randomNumber == 3) {
     console.log('Wygrałeś');
 
 }
-else if (player == 2 && randomNumber == 1){
+else if (player == 2 && randomNumber == 1) {
     console.log('Wygrałeś bitwę');
 }
-else if (player == 3 && randomNumber == 2){
+else if (player == 3 && randomNumber == 2) {
     console.log('Wygrałeś bitwę');
 }
 else {
     console.log("Komputer wygrał bitwę");
 }
+
+
+
 
 
 
