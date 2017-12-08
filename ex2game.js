382157ac01e1816ab4d6c4c58a77808caac98d78
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-08 19:05:55 +0100

    Add variables and fix random number. Now it is pick number form 1 to 3.

--- ex2game.js
+++ ex2game.js
@@ -1,5 +1,10 @@
-var randomNumber = Math.ceil(Math.random() * 10);
-var player = prompt('Podaj liczbę od 1 do 3. Legenda: 1= papier, 2= nożyce, 3= kamień');
+var randomNumber = Math.floor(Math.random() * 3 + 1),
+    player = prompt('Podaj liczbę od 1 do 3. Legenda: 1= papier, 2= nożyce, 3= kamień'),
+    paper = 1,
+    scissors = 2,
+    stone = 3,
+    winner = 'Wygrałeś bitwę!',
+    loser = 'Przegrałeś bitwe';
 
 if (player > 3) {
     alert('Podałeś złą liczbę');
@@ -7,23 +12,47 @@ if (player > 3) {
 
 }
 
+if(randomNumber == paper){
+    console.log('Komputer wybrał: papier');
 
-console.log('Komputer podał liczbę: ' + randomNumber);
+}
+else if(randomNumber == scissors){
+    console.log('Komputer wybrał: nożyce');
 
-console.log('Podałeś liczbę: ' + player);
+}
+else if(randomNumber == stone){
+    console.log('Komputer wybrał: kamień');
+
+}
+
+if(player == paper){
+    console.log('Gracz wybrał: papier');
+
+}
+else if(player == scissors){
+    console.log('Gracz wybrał: nożyce');
+
+}
+else if(player == stone){
+    console.log('Gracz wybrał: kamień');
+
+}
 
 if (player == 1 && randomNumber == 3) {
-    console.log('Wygrałeś');
+    console.log(winner);
 
 }
 else if (player == 2 && randomNumber == 1) {
-    console.log('Wygrałeś bitwę');
+    console.log(winner);
 }
 else if (player == 3 && randomNumber == 2) {
-    console.log('Wygrałeś bitwę');
+    console.log(winner);
+}
+else if (player == randomNumber){
+    console.log('Nikt nie wygrał!')
 }
 else {
-    console.log("Komputer wygrał bitwę");
+    console.log(loser);
 }
 
 
