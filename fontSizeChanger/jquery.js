1470128d9e612f8d692c69ad90c288aa43357b4f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-14 11:25:54 +0100

    Fix in fontSizeChanger

--- fontSizeChanger/jquery.js
+++ fontSizeChanger/jquery.js
@@ -6,22 +6,25 @@ $(function () {
     var $buttons = $('.buttons');
     var $text = $('.text');
     var $fontSizeMenuSlider = $('.font-size-change-slider')
-
+    var $title = $('h1');
 
     $fonSizeBtnUp.on('click', function () {
-        $text.css('font-size', '30px');
+        $text.css('font-size', '2em');
 
     });
     $fonSizeBtnNormal.on('click', function () {
-        $text.css('font-size', '20px');
+        $text.css('font-size', '1em');
 
     });
     $fonSizeBtnDown.on('click', function () {
-        $text.css('font-size', '10px');
+        $text.css('font-size', '0.5em');
 
     });
     $fontSizeMenuSlider.on('click', function () {
         $buttons.slideToggle(600);
     })
+
+    $title.animate({left: "10px"}, 600);
+    $text.animate({right: '0px'}, 1200);
 
 });
